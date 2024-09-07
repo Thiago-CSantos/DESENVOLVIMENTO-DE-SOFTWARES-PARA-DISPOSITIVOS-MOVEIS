9796b9c81e10367ea7537abef777e31700be596f
Thiago Carretero <thiagocarretero433@gmail.com>
2024-09-06 21:48:09 -0300

    projeto app

--- MovieApp/app/src/main/java/com/thiago/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/thiago/movieapp/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.thiago.movieapp
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Toast.makeText(this, "App de filmes", Toast.LENGTH_SHORT).show();
 
         setContentView(R.layout.activity_main)
+
+        // Trazer o obj Kotlin de um obj visual do xml
+        val btn: Button = findViewById<Button>(R.id.btnEnviar)
+
+        btn.setOnClickListener{
+            val nome = findViewById<EditText>(R.id.editName)
+            nome.setText("Meu nome mudou pelo main")
+            Toast.makeText(this, "App de filmes", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
